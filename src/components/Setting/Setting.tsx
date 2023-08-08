82dc8937b7dbe5fe5b616b6cc5737c0734df27c1
mahererais <raismaher.store@gmail.com>
2023-08-08 22:51:40 +0200

    import Fonts - Poppins from url & Robot from file .woff2

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
-import './Setting.scss';
-import { IoSettingsOutline } from 'react-icons/io5';
+import React, { useCallback, useState } from "react";
+import "./Setting.scss";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const Setting = () => {
+  const [displayModal, setDisplayModal] = useState(true);
 
-    const [isHide, setHide] = useState(true);
+  const onClick = useCallback(() => {
+    setDisplayModal((prevStat) => !prevStat);
+  }, []);
 
-    const onClick = () => { 
-        setHide(prevStat => !prevStat) 
-    };
-    const onSubmit = (e: React.MouseEvent<HTMLFormElement>) => { 
-        e.preventDefault();
-        
-        
-     };
+  const onSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
-    return (
-        <div className='setting_container'>
-            <IoSettingsOutline onClick={onClick}/>
-            <form className={isHide ? 'hide' : ''} onSubmit={onSubmit} >
-                <input type="email" name="email" id="email" placeholder='Entrez votre email'/>
-                <input type="password" name="password" id="password" placeholder='Entrez votre mot de passe'/>
-                <button type="submit">Envoyer</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="setting_container">
+      <IoSettingsOutline onClick={onClick} />
+      <form className={displayModal ? "hide" : ""} onSubmit={onSubmit}>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Entrez votre email"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Entrez votre mot de passe"
+        />
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
 };
 
 export default Setting;
