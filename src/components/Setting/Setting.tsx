16dd66871450cc7b74b6f3d01ef754a5082b8e66
mahererais <raismaher.store@gmail.com>
2023-08-08 22:23:54 +0200

    preventDefault pour le formulaire

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Setting.scss';
 import { IoSettingsOutline } from 'react-icons/io5';
 
@@ -6,12 +6,19 @@ const Setting = () => {
 
     const [isHide, setHide] = useState(true);
 
-    const toggle = () => { setHide(prevStat => !prevStat) };
+    const onClick = () => { 
+        setHide(prevStat => !prevStat) 
+    };
+    const onSubmit = (e: React.MouseEvent<HTMLFormElement>) => { 
+        e.preventDefault();
+        
+        
+     };
 
     return (
         <div className='setting_container'>
-            <IoSettingsOutline onClick={toggle}/>
-            <form className={isHide ? 'hide' : ''} >
+            <IoSettingsOutline onClick={onClick}/>
+            <form className={isHide ? 'hide' : ''} onSubmit={onSubmit} >
                 <input type="email" name="email" id="email" placeholder='Entrez votre email'/>
                 <input type="password" name="password" id="password" placeholder='Entrez votre mot de passe'/>
                 <button type="submit">Envoyer</button>
