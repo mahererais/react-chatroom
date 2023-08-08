d2ed265bb42f9c8ee2806d455cc831b0ed71cad2
mahererais <raismaher.store@gmail.com>
2023-08-08 18:38:35 +0200

    animation du formulaire de connexion lors du click

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './Setting.scss';
 import { IoSettingsOutline } from 'react-icons/io5';
 
 const Setting = () => {
 
+    const [isHide, setHide] = useState(true);
+
+    const toggle = () => { setHide(prevStat => !prevStat) };
+
     return (
         <div className='setting_container'>
-            <IoSettingsOutline />
-            <form>
+            <IoSettingsOutline onClick={toggle}/>
+            <form className={isHide ? 'hide' : ''} >
                 <input type="email" name="email" id="email" placeholder='Entrez votre email'/>
                 <input type="password" name="password" id="password" placeholder='Entrez votre mot de passe'/>
                 <button type="submit">Envoyer</button>
