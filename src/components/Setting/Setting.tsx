3aeac5c74b39b2f80d167353ac5ae6d7e4574721
mahererais <raismaher.store@gmail.com>
2023-08-09 13:22:20 +0200

    connexion a mon api  endpoint => /login

--- src/components/Setting/Setting.tsx
+++ src/components/Setting/Setting.tsx
@@ -4,14 +4,43 @@ import { IoSettingsOutline } from "react-icons/io5";
 
 const Setting = () => {
   const [displayModal, setDisplayModal] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onClick = useCallback(() => {
     setDisplayModal((prevStat) => !prevStat);
   }, []);
 
-  const onSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+  // const onSubmit = (e: React.MouseEvent<HTMLFormElement>) => {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify( {
+        email: email,
+        password: password,
+      })
+    })
+      .then ( response =>  response.json())
+      .then (data => {
+        console.log(data);
+
+      })
+      .catch(error => console.error(error));
+      
+
   };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement> | React.ClipboardEvent<HTMLInputElement>) => {
+    setEmail((e.target as HTMLInputElement).value);
+  }
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement> | React.ClipboardEvent<HTMLInputElement>) => {
+    setPassword((e.target as HTMLInputElement).value);
+  }
 
   return (
     <div className="setting_container">
@@ -22,12 +51,24 @@ const Setting = () => {
           name="email"
           id="email"
           placeholder="Entrez votre email"
-        />
+          required
+          value={email}
+          onChange={handleEmailChange}
+          onPaste={handleEmailChange}
+          onCut={handleEmailChange}
+          onCopy={handleEmailChange}
+          />
         <input
           type="password"
           name="password"
           id="password"
           placeholder="Entrez votre mot de passe"
+          required
+          value={password}
+          onChange={handlePasswordChange}
+          onPaste={handlePasswordChange}
+          onCut={handlePasswordChange}
+          onCopy={handlePasswordChange}
         />
         <button type="submit">Envoyer</button>
       </form>
